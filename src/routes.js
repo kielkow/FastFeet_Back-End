639cc50f3a9c5b6948ceb6464b73815bd7265386
kielkow <matheuskiel@fiorifer.com.br>
2020-02-27 20:37:16 -0300

    Create models Loader

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.send({ messsage: 'Hello World' }));
+routes.post('/users', async (req, res) => {
+  const user = await User.create({
+    name: 'kielkow',
+    email: 'matheuskiel@fiorifer.com.br',
+    password_hash: '123456',
+    provider: true,
+  });
 
+  return res.json(user);
+});
 export default routes;
