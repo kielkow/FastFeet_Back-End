40b9ddace7beb74f697b2dbb8501f9e5566247b4
kielkow <matheuskiel@fiorifer.com.br>
2020-02-27 21:05:34 -0300

    Create route store user && Create store function on UserController

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.post('/users', async (req, res) => {
-  const user = await User.create({
-    name: 'kielkow',
-    email: 'matheuskiel@fiorifer.com.br',
-    password_hash: '123456',
-    provider: true,
-  });
+routes.post('/users', UserController.store);
 
-  return res.json(user);
-});
 export default routes;
