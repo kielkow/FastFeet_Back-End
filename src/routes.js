3df0c8eac3d4f476a86e533cbf8ca7fa0080cf3a
kielkow <matheuskiel@fiorifer.com.br>
2020-02-28 16:43:09 -0300

    Update User

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,16 @@ import UserController from './app/controllers/UserController';
 import RecipientController from './app/controllers/RecipientController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 routes.post('/recipients/:id', RecipientController.store);
 
