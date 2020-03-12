ade3104dd6420d6d005620a753094f8c74060dcc
kielkow <matheuskiel@fiorifer.com.br>
2020-03-12 19:46:37 -0300

    Config Enviroment Variables

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'b1a139df820ca4',
-    pass: 'eb424112315979',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe FastFeet <noreply@fastfeet.com>',
